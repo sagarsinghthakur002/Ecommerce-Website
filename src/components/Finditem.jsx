ef4153e616f6b3cedd7279d3aec1373041d67bef
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-10-25 23:38:26 +0545

    feat: email

--- src/components/Finditem.jsx
+++ src/components/Finditem.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const FInditem = () => {
   return (
-    <div>
-      
+    <div data-aos="zoom-in" className='mb-20 bg-slate-700 dark:bg-gray-800 text-white'> 
+      <div className='conrainer backdrop-blur-sm py-10'>
+        <div className='space-y-6 max-w-xl mx-auto'>
+            <h2 className='text-2xl text-center sm:text-left sm:text-4xl font-semibold'>Get Notified </h>
+            <input type='email' data-aos="fade-up" placeholder='enter your email'></input>
+        </div>
+      </div>
     </div>
   )
 }
