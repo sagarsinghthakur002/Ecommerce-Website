3c7816400e11ac15d4308817166c093464fcdfc4
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-10-30 00:00:26 +0545

    feat:footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { FiShoppingBag } from 'react-icons/fi'
+import { FaMapLocation } from 'react-icons/fa6'
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaMobileAlt,
+  FaLocationArrow,
+} from 'react-icons/fa'
+import { IoCall } from 'react-icons/io5'
+
+const FooterLinks = [
+  {
+    title:"Home"
+    link:"/#"
+  },
+
+  {
+    title:"About"
+    link:"/#"
+  },
+
+  {
+    title:"Contact"
+    link:"/#"
+  },
+
+  {
+    title:"Blog"
+    link:"/#"
+  },
+]
 
 function Footer() {
   return (
-    <div>
+    <div className='text-white bg-[#212529]'>
+      <div className='container'>
+        <div data-aos="zoom-in"
+        className='grid md:grid-cols-3 pb-44 pt-5'>
+          <div className='py-8 px-4'>
+            <h1 className='text-2xl font-bold sm:text-left text-justify mb-3 items-center gap-1'></h1>
+          </div>
+
+        </div>
+      </div>
       
     </div>
   )
